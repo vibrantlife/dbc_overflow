c98247c84278376281b3f5222fa354fced21a0e8
vibrantlife <nicholesvibrantlife@gmail.com>
2015-03-17 17:29:14 -0700

    styling. added upvote and downvote buttons

---
+++ dbc_overflow/db/migrate/20150318001806_add_vote_counts_to_questions.rb
@@ -0,0 +1,5 @@
+class AddVoteCountsToQuestions < ActiveRecord::Migration
+  def change
+  	add_column :questions, :vote_count, :integer, :default => 0
+  end
+end
--- dbc_overflow/db/schema.rb
+++ dbc_overflow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317014150) do
+ActiveRecord::Schema.define(version: 20150318001806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20150317014150) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "vote_count", default: 0
   end
 
 end
