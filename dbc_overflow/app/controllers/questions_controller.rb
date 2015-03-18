7eb81c7a43fc71d27070483b463a46fb715e3260
Vibrantlife <nicholesvibrantlife@gmail.com>
2015-03-18 16:40:51 -0700

    updated controller so answers is defaulted to 0. fixed weird routing

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -20,7 +20,7 @@ class QuestionsController < ApplicationController
   def create
   	@question = Question.new(question_params)
   	if @question.save
-  		redirect_to @question
+  		redirect_to root_path
   	else
   		render 'new'
   	end
--- dbc_overflow/db/migrate/20150318033121_add_votes_to_answers.rb
+++ dbc_overflow/db/migrate/20150318033121_add_votes_to_answers.rb
@@ -1,5 +1,5 @@
 class AddVotesToAnswers < ActiveRecord::Migration
   def change
-    add_column :answers, :vote_count, :integer
+    add_column :answers, :vote_count, :integer :default => 0
   end
 end
